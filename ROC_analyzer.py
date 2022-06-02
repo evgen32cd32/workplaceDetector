620e19e5a07c0e155d3dda0cdf1ccfd6ce0490ad
Evgenii Khvatov <evgen32cd32@gmail.com>
2022-06-02 18:49:35 +0300

    Update ROC_analyzer.py

--- ROC_analyzer.py
+++ ROC_analyzer.py
@@ -8,6 +8,8 @@ ROC = pd.read_csv(rootpath + '/ROC.csv');
 ROC.index = ROC.iloc[:,0];
 ROC = ROC.iloc[:,1:];
 
+ROC_AUC_17 = - np.trapz(ROC['tpr17'],ROC['fpr17']);
+ROC_AUC_19 = - np.trapz(ROC['tpr19'],ROC['fpr19']);
 
 
 plt.plot(ROC['fpr17'],ROC['tpr17'], label = "17");
